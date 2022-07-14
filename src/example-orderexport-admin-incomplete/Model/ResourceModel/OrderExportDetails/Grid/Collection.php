2ac34e4bb548328655fe0f74053a762f862400ef
Chris Nanninga <tarsusc81@gmail.com>
2022-07-14 14:48:55 -0500

    Copy incomplete version of admin module

---
+++ src/example-orderexport-admin-incomplete/Model/ResourceModel/OrderExportDetails/Grid/Collection.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types = 1);
+/**
+ * @by SwiftOtter, Inc.
+ * @website https://swiftotter.com
+ **/
+
+namespace SwiftOtter\OrderExportAdmin\Model\ResourceModel\OrderExportDetails\Grid;
+
+use Magento\Framework\Api\Search\AggregationInterface;
+use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\Event\ManagerInterface;
+use SwiftOtter\OrderExport\Model\ResourceModel\OrderExportDetails\Collection as OrderExportDetailsCollection;
+use SwiftOtter\OrderExport\Model\ResourceModel\OrderExportDetails as OrderExportDetailsResource;
+
+class Collection extends OrderExportDetailsCollection implements SearchResultInterface
+{
+    /**
+     * @var AggregationInterface
+     */
+    protected $aggregations;
+
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        $model = \Magento\Framework\View\Element\UiComponent\DataProvider\Document::class,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_init($model, OrderExportDetailsResource::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+}
