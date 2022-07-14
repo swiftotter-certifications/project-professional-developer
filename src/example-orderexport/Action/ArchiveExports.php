15fd9e0068e30e3b41a340e31e9dc90a12d24344
Chris Nanninga <tarsusc81@gmail.com>
2022-07-14 16:10:47 -0500

    Add cron job

---
+++ src/example-orderexport/Action/ArchiveExports.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+/**
+ * @by SwiftOtter, Inc.
+ * @website https://swiftotter.com
+ **/
+
+namespace SwiftOtter\OrderExport\Action;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use SwiftOtter\OrderExport\Model\OrderExportDetailsRepository;
+
+class ArchiveExports
+{
+    const EXPIRATION_DAYS = 30;
+
+    /**
+     * @var OrderExportDetailsRepository
+     */
+    private $orderExportDetailsRepository;
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    public function __construct(
+        OrderExportDetailsRepository $orderExportDetailsRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        $this->orderExportDetailsRepository = $orderExportDetailsRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function execute(): ArchiveExports
+    {
+        $expDate = (new \DateTime())
+            ->setTimezone(new \DateTimeZone('UTC'))
+            ->sub(new \DateInterval('P' . self::EXPIRATION_DAYS . 'D'));
+
+        $this->searchCriteriaBuilder->addFilter('archived', 0)
+            ->addFilter('exported_at', $expDate->format('Y-m-d H:i:s'), 'lt');
+        $archiveOrders = $this->orderExportDetailsRepository->getList($this->searchCriteriaBuilder->create())->getItems();
+
+        foreach ($archiveOrders as $order) {
+            $order->setArchived(true);
+            $this->orderExportDetailsRepository->save($order);
+        }
+
+        return $this;
+    }
+}
