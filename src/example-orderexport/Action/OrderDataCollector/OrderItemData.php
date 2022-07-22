5d780e9c9ccdf126624a06742357586427fd8786
Chris Nanninga <tarsusc81@gmail.com>
2022-07-22 08:59:05 -0500

    Add sku_override to item export data

--- src/example-orderexport/Action/OrderDataCollector/OrderItemData.php
+++ src/example-orderexport/Action/OrderDataCollector/OrderItemData.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
 
 namespace SwiftOtter\OrderExport\Action\OrderDataCollector;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\Data\OrderItemInterface;
 use SwiftOtter\OrderExport\Action\GetOrderExportItems;
@@ -19,19 +22,39 @@ class OrderItemData implements OrderDataCollectorInterface
      * @var GetOrderExportItems
      */
     private $getOrderExportItems;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
 
     public function __construct(
-        GetOrderExportItems $getOrderExportItems
+        GetOrderExportItems $getOrderExportItems,
+        ProductRepositoryInterface $productRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder
     ) {
         $this->getOrderExportItems = $getOrderExportItems;
+        $this->productRepository = $productRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
     }
 
     public function collect(OrderInterface $order, HeaderData $headerData): array
     {
-        $items = [];
+        $filteredOrderItems = $this->getOrderExportItems->execute($order);
 
-        foreach ($this->getOrderExportItems->execute($order) as $item) {
-            $items[] = $this->transform($item);
+        $skus = [];
+        foreach ($filteredOrderItems as $item) {
+            $skus[] = $item->getSku();
+        }
+        $productsBySku = $this->loadProducts($skus);
+
+        $items = [];
+        foreach ($filteredOrderItems as $item) {
+            $product = $productsBySku[$item->getSku()] ?? null;
+            $items[] = $this->transform($item, $product);
         }
 
         return [
@@ -39,14 +62,43 @@ class OrderItemData implements OrderDataCollectorInterface
         ];
     }
 
-    private function transform(OrderItemInterface $orderItem): array
+    private function transform(OrderItemInterface $orderItem, ?ProductInterface $product): array
     {
         return [
-            'sku' => $orderItem->getSku(),
+            'sku' => $this->getSku($orderItem, $product),
             'qty' => $orderItem->getQtyOrdered(),
             'item_price' => $orderItem->getBasePrice(),
             'item_cost' => $orderItem->getBaseCost(),
             'total' => $orderItem->getBaseRowTotal()
         ];
+    }
+
+    private function loadProducts(array $skus): array
+    {
+        $this->searchCriteriaBuilder->addFilter('sku', $skus, 'in');
+        /** @var ProductInterface[] $products */
+        $products = $this->productRepository->getList($this->searchCriteriaBuilder->create())->getItems();
+
+        $productsBySku = [];
+        foreach ($products as $product) {
+            $productsBySku[$product->getSku()] = $product;
+        }
+        return $productsBySku;
+    }
+
+    private function getSku(OrderItemInterface $orderItem, ?ProductInterface $product): string
+    {
+        $sku = $orderItem->getSku();
+        if ($product === null) {
+            return $sku;
+        }
+
+        $skuOverride = $product->getCustomAttribute('sku_override');
+        $skuOverrideVal = ($skuOverride !== null) ? $skuOverride->getValue() : null;
+
+        if (!empty($skuOverrideVal)) {
+            $sku = $skuOverrideVal;
+        }
+        return $sku;
     }
 }
