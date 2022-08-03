454bbc3d485b293b305e14878a2c3e853aa25bc9
Chris Nanninga <tarsusc81@gmail.com>
2022-08-03 17:29:05 -0500

    Step 3.1.2

--- src/example-orderexport/Model/Config.php
+++ src/example-orderexport/Model/Config.php
@@ -13,6 +13,8 @@ use Magento\Store\Model\ScopeInterface;
 class Config
 {
     const CONFIG_PATH_ENABLED = 'sales/order_export/enabled';
+    const CONFIG_PATH_API_TOKEN = 'sales/order_export/api_token';
+    const CONFIG_PATH_API_URL = 'sales/order_export/api_url';
 
     /**
      * @var ScopeConfigInterface
@@ -28,5 +30,17 @@ class Config
     public function isEnabled(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): bool
     {
         return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_ENABLED, $scopeType, $scopeCode);
+    }
+
+    public function getApiToken(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_TOKEN, $scopeType, $scopeCode);
+        return ($value !== null) ? (string) $value : '';
+    }
+
+    public function getApiUrl(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): string
+    {
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_API_URL, $scopeType, $scopeCode);
+        return ($value !== null) ? (string) $value : '';
     }
 }
