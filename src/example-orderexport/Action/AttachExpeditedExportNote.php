decbbe63e62ff6b2cd15a58ae55b9d5c4b41b7d8
Chris Nanninga <tarsusc81@gmail.com>
2022-08-09 14:40:21 -0500

    Move observer logic to action class

---
+++ src/example-orderexport/Action/AttachExpeditedExportNote.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+/**
+ * @by SwiftOtter, Inc.
+ * @website https://swiftotter.com
+ **/
+
+namespace SwiftOtter\OrderExport\Action;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Store\Model\ScopeInterface;
+use Psr\Log\LoggerInterface;
+use SwiftOtter\OrderExport\Api\Data\OrderExportDetailsInterface;
+use SwiftOtter\OrderExport\Api\Data\OrderExportDetailsInterfaceFactory;
+use SwiftOtter\OrderExport\Api\OrderExportDetailsRepositoryInterface;
+use SwiftOtter\OrderExport\Model\Config;
+
+class AttachExpeditedExportNote
+{
+    const EXPEDITED_MSG = 'Order contains expedited items';
+
+    /** @var Config */
+    private $config;
+    /** @var GetOrderExportItems */
+    private $getOrderExportItems;
+    /** @var OrderExportDetailsInterfaceFactory */
+    private $exportDetailsFactory;
+    /** @var OrderExportDetailsRepositoryInterface */
+    private $exportDetailsRepository;
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(
+        Config $config,
+        GetOrderExportItems $getOrderExportItems,
+        OrderExportDetailsInterfaceFactory $exportDetailsFactory,
+        OrderExportDetailsRepositoryInterface $exportDetailsRepository,
+        LoggerInterface $logger
+    ) {
+        $this->config = $config;
+        $this->getOrderExportItems = $getOrderExportItems;
+        $this->exportDetailsFactory = $exportDetailsFactory;
+        $this->exportDetailsRepository = $exportDetailsRepository;
+        $this->logger = $logger;
+    }
+
+    public function execute(OrderInterface $order): void
+    {
+        if ($this->config->isEnabled(ScopeInterface::SCOPE_STORE, $order->getStoreId())) {
+            return;
+        }
+
+        $expeditedSkus = $this->config->getExpeditedSkus(ScopeInterface::SCOPE_STORE, (string) $order->getStoreId());
+        if (empty($expeditedSkus)) {
+            return;
+        }
+
+        $expedited = false;
+        foreach ($this->getOrderExportItems->execute($order) as $item) {
+            if (in_array($item->getSku(), $expeditedSkus)) {
+                $expedited = true;
+                break;
+            }
+        }
+
+        if ($expedited) {
+            try {
+                $orderExts = $order->getExtensionAttributes();
+                /** @var OrderExportDetailsInterface $exportDetails */
+                $exportDetails = $orderExts->getExportDetails();
+                if (!$exportDetails) {
+                    $exportDetails = $this->exportDetailsFactory->create();
+                    $exportDetails->setOrderId((int)$order->getEntityId());
+                    $orderExts->setExportDetails($exportDetails);
+                }
+
+                $exportDetails->setMerchantNotes((string)__(self::EXPEDITED_MSG));
+                $this->exportDetailsRepository->save($exportDetails);
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
+        }
+    }
+}
--- src/example-orderexport/Observer/SalesOrder/AttachExpeditedExportNote.php
+++ src/example-orderexport/Observer/SalesOrder/AttachExpeditedExportNote.php
@@ -10,44 +10,17 @@ namespace SwiftOtter\OrderExport\Observer\SalesOrder;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\Data\OrderInterface;
-use Magento\Store\Model\ScopeInterface;
-use SwiftOtter\OrderExport\Action\GetOrderExportItems;
-use SwiftOtter\OrderExport\Api\Data\OrderExportDetailsInterface;
-use SwiftOtter\OrderExport\Api\Data\OrderExportDetailsInterfaceFactory;
-use SwiftOtter\OrderExport\Api\OrderExportDetailsRepositoryInterface;
-use SwiftOtter\OrderExport\Model\Config;
+use SwiftOtter\OrderExport\Action\AttachExpeditedExportNote as AttachExpeditedNoteAction;
 
 class AttachExpeditedExportNote implements ObserverInterface
 {
-    const EXPEDITED_MSG = 'Order contains expedited items';
-
-    /**
-     * @var Config
-     */
-    private $config;
-    /**
-     * @var GetOrderExportItems
-     */
-    private $getOrderExportItems;
-    /**
-     * @var OrderExportDetailsInterfaceFactory
-     */
-    private $orderExportDetailsFactory;
-    /**
-     * @var OrderExportDetailsRepositoryInterface
-     */
-    private $orderExportDetailsRepository;
+    /** @var AttachExpeditedNoteAction */
+    private $attachExpeditedExportNote;
 
     public function __construct(
-        Config $config,
-        GetOrderExportItems $getOrderExportItems,
-        OrderExportDetailsInterfaceFactory $orderExportDetailsFactory,
-        OrderExportDetailsRepositoryInterface $orderExportDetailsRepository
+        AttachExpeditedNoteAction $attachExpeditedExportNote
     ) {
-        $this->config = $config;
-        $this->getOrderExportItems = $getOrderExportItems;
-        $this->orderExportDetailsFactory = $orderExportDetailsFactory;
-        $this->orderExportDetailsRepository = $orderExportDetailsRepository;
+        $this->attachExpeditedExportNote = $attachExpeditedExportNote;
     }
 
     /**
@@ -55,45 +28,12 @@ class AttachExpeditedExportNote implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        try {
-            if (!$this->config->isEnabled()) {
-                return;
-            }
-
-            /** @var OrderInterface $order */
-            $order = $observer->getEvent()->getData('order');
-            if (!$order) {
-                return;
-            }
-
-            $expeditedSkus = $this->config->getExpeditedSkus(ScopeInterface::SCOPE_STORE, (string) $order->getStoreId());
-            if (empty($expeditedSkus)) {
-                return;
-            }
-
-            $expedited = false;
-            foreach ($this->getOrderExportItems->execute($order) as $item) {
-                if (in_array($item->getSku(), $expeditedSkus)) {
-                    $expedited = true;
-                    break;
-                }
-            }
-
-            if ($expedited) {
-                $orderExts = $order->getExtensionAttributes();
-                $exportDetails = $orderExts->getExportDetails();
-                if (!$exportDetails) {
-                    /** @var OrderExportDetailsInterface $exportDetails */
-                    $exportDetails = $this->orderExportDetailsFactory->create();
-                    $exportDetails->setOrderId((int) $order->getEntityId());
-                    $orderExts->setExportDetails($exportDetails);
-                }
-
-                $exportDetails->setMerchantNotes((string) __(self::EXPEDITED_MSG));
-                $this->orderExportDetailsRepository->save($exportDetails);
-            }
-        } catch (\Throwable $e) {
-            // TODO Logging or re-try behavior
+        /** @var OrderInterface $order */
+        $order = $observer->getEvent()->getData('order');
+        if (!$order) {
+            return;
         }
+
+        $this->attachExpeditedExportNote->execute($order);
     }
 }
