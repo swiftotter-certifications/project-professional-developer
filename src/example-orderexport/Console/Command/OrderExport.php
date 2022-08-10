d7c5902e41374dbce12231d1643d8d2548d179d2
Chris Nanninga <tarsusc81@gmail.com>
2022-08-10 16:53:40 -0500

    Step 2.1.3

--- src/example-orderexport/Console/Command/OrderExport.php
+++ src/example-orderexport/Console/Command/OrderExport.php
@@ -8,18 +8,41 @@ declare(strict_types=1);
 namespace SwiftOtter\OrderExport\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class OrderExport extends Command
 {
+    const ARG_NAME_ORDER_ID = 'order-id';
+    const OPT_NAME_SHIP_DATE = 'ship-date';
+    const OPT_NAME_MERCHANT_NOTES = 'notes';
+
     /**
      * @inheritdoc
      */
     protected function configure()
     {
         $this->setName('order-export:run')
-            ->setDescription('Export order to ERP');
+            ->setDescription('Export order to ERP')
+            ->addArgument(
+                self::ARG_NAME_ORDER_ID,
+                InputArgument::REQUIRED,
+                "Order ID"
+            )
+            ->addOption(
+                self::OPT_NAME_SHIP_DATE,
+                'd',
+                InputOption::VALUE_OPTIONAL,
+                'Shipping date in format YYYY-MM-DD'
+            )
+            ->addOption(
+                self::OPT_NAME_MERCHANT_NOTES,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Merchant notes'
+            );
     }
 
     /**
@@ -29,6 +52,8 @@ class OrderExport extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln('Hello world, from a CLI command!');
+
         return 0;
     }
 }
