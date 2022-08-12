ef9a6a7521124b4f99e5f5a948fcafe093bbadfc
Chris Nanninga <tarsusc81@gmail.com>
2022-08-12 17:39:44 -0500

    Step 3.2.2

--- src/example-orderexport/Action/PushDetailsToWebservice.php
+++ src/example-orderexport/Action/PushDetailsToWebservice.php
@@ -10,6 +10,7 @@ namespace SwiftOtter\OrderExport\Action;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Store\Model\ScopeInterface;
 use SwiftOtter\OrderExport\Model\Config;
+use GuzzleHttp\Client;
 
 class PushDetailsToWebservice
 {
@@ -26,6 +27,17 @@ class PushDetailsToWebservice
     {
         $apiUrl = $this->config->getApiUrl(ScopeInterface::SCOPE_STORE, $order->getStoreId());
         $apiToken = $this->config->getApiToken(ScopeInterface::SCOPE_STORE, $order->getStoreId());
+
+        $client = new Client();
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $apiToken,
+            ],
+            'body' => \json_encode($exportDetails),
+        ];
+
+        $client->post($apiUrl, $options);
 
         // TODO Make an HTTP request
 
