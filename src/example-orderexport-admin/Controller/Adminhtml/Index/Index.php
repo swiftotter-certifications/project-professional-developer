8a7da7271dccebfceec1e9f1e00123d7cc74f579
Chris Nanninga <tarsusc81@gmail.com>
2022-08-16 10:08:55 -0500

    Step 7.2.1

--- src/example-orderexport-admin/Controller/Adminhtml/Index/Index.php
+++ src/example-orderexport-admin/Controller/Adminhtml/Index/Index.php
@@ -15,7 +15,7 @@ use Magento\Framework\View\Result\PageFactory;
 
 class Index extends BackendAction implements HttpGetActionInterface
 {
-    const ADMIN_RESOURCE = '{RESOURCE}';
+    const ADMIN_RESOURCE = 'SwiftOtter_OrderExport::OrderExport';
 
     /**
      * @var PageFactory
